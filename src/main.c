e44192238d2837fa58a705950e3846fc8ae82ed6
tgnlex <alexanderwebber76@gmail.com>
2025-01-05 06:32:01 -0500

    updated editorProcessKeypress(), set keybinding for editorFind() [search] to CTRL+f

--- src/main.c
+++ src/main.c
@@ -599,6 +599,9 @@ void editorProcessKeypress() {
       if (E.cy < E.numrows) 
         E.cx = E.row[E.cy].size;
       break;
+    case CTRL_KEY('f'):
+      editorFind();
+      break;
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY: 
